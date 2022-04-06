07d4258aa928e8561267fdc1a69771dfaf641372
dboo <dboo.studio@gmail.com>
2022-04-06 19:04:02 +0900

    챕터별 main.js를 수행할 수 있도록 구조 변경

---
+++ chapter1/main.js
@@ -0,0 +1,55 @@
+const {statement} = require("./statement");
+module.exports = {
+    chapter1: function () {
+        testStatement();
+    }
+}
+
+function testStatement() {
+    let statementResult = statement(testData().invoices, testData().plays);
+    test(statementResult);
+}
+
+function testData(){
+    let data = {};
+    let plays = {
+        "hamlet": {"name": "Hamlet", "type": "tragedy"},
+        "as-like": {"name": "As You Like It", "type": "comedy"},
+        "othello": {"name": "Othello", "type": "tragedy"},
+    }
+    let invoices = {
+        "customer": "BigCo",
+        "performances": [
+            {
+                "playID": "hamlet",
+                "audiences": 55
+            },
+            {
+                "playID": "as-like",
+                "audiences": 35
+            }
+        ]
+    }
+    data.plays = plays;
+    data.invoices = invoices;
+    return data;
+}
+
+function test(statementResult){
+    let forTestResult = "청구 내역 (고객명: BigCo)\n" +
+        "  Hamlet: $0.00 (55석)\n" +
+        "  As You Like It: $0.00 (35석)\n" +
+        "총액: $0.00\n" +
+        "적립 포인트: 37점\n";
+
+    console.log(statementResult);
+
+    if(statementResult != forTestResult){
+        console.log("------------")
+        console.log("테스트 실패")
+        throw "테스트 실패";
+    } else {
+        console.log("------------")
+        console.log("테스트 성공")
+    }
+}
--- index.js
+++ index.js
@@ -1,43 +1,4 @@
-const {statement} = require("./statement");
-
-let plays = {
-    "hamlet": {"name": "Hamlet", "type": "tragedy"},
-    "as-like": {"name": "As You Like It", "type": "comedy"},
-    "othello": {"name": "Othello", "type": "tragedy"},
-}
-
-let invoices =
-{
-    "customer": "BigCo",
-    "performances": [
-        {
-            "playID": "hamlet",
-            "audiences": 55
-        },
-        {
-            "playID": "as-like",
-            "audiences": 35
-        }
-    ]
-}
-
-let statementResult = statement(invoices, plays);
-
-let forTestResult = "청구 내역 (고객명: BigCo)\n" +
-    "  Hamlet: $0.00 (55석)\n" +
-    "  As You Like It: $0.00 (35석)\n" +
-    "총액: $0.00\n" +
-    "적립 포인트: 37점\n";
-
-console.log(statementResult);
-
-if(statementResult != forTestResult){
-    console.log("------------")
-    console.log("테스트 실패")
-    throw "테스트 실패";
-} else {
-    console.log("------------")
-    console.log("테스트 성공")
-}
+const {chapter1} = require("./chapter1/main");
 
 
+chapter1();
