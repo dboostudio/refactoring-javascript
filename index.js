4f53b4db5f00e9521f318dbf3f9652a4bb070979
dboo <dboo.studio@gmail.com>
2022-04-06 15:39:14 +0900

    결과 테스트 코드 추가(index.js)

--- index.js
+++ index.js
@@ -23,6 +23,21 @@ let invoices =
 
 let statementResult = statement(invoices, plays);
 
+let forTestResult = "청구 내역 (고객명: BigCo)\n" +
+    "  Hamlet: $0.00 (55석)\n" +
+    "  As You Like It: $0.00 (35석)\n" +
+    "총액: $0.00\n" +
+    "적립 포인트: 37점\n";
+
 console.log(statementResult);
+
+if(statementResult != forTestResult){
+    console.log("------------")
+    console.log("테스트 실패")
+    throw "테스트 실패";
+} else {
+    console.log("------------")
+    console.log("테스트 성공")
+}
 
 
