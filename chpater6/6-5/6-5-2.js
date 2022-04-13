e7dc801bb742c668292a53678d2da02dfec59608
dboo <dboo.studio@gmail.com>
2022-04-13 18:17:43 +0900

    ch6 중간커밋

---
+++ chpater6/6-5/6-5-2.js
@@ -0,0 +1,5 @@
+// 예시: 함수 이름 바꾸기 (간단한 절차)
+
+function circum(radius){ return 2 * Math.PI * radius}
+
+circum(3);
---
+++ chpater6/6-5/6-5-3.js
@@ -0,0 +1,5 @@
+// 예시: 함수 이름 바꾸기 (마이그레이션)
+
+function circum(radius){ return 2 * Math.PI * radius}
+
+circum(3);
